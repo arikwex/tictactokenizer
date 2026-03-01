059e0ade42aa88e7f4789e93950abc43e64a5977
Ariel Wexler <ariel@hedra.com>
2026-03-01 11:57:51 -0500

    public build dir

--- rollup.config.js
+++ rollup.config.js
@@ -200,7 +200,7 @@ const baseCss = `
 module.exports = {
   input: "tictactokenizer_game.js",
   output: {
-    file: "dist/bundle.js",
+    file: "public/bundle.js",
     format: "iife",
     sourcemap: false,
   },
